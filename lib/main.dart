eb63bbb27a7f6432c81a3c9a5935ef4b7a3d0d85
BrokeOnCrypto <47540864+BrokeOnCrypto@users.noreply.github.com>
2021-04-15 20:53:14 -0500

    Custom widget implemented
    StyledTextButton widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:html';
+import 'package:http/http.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
@@ -31,11 +33,66 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Client httpClient;
   Web3Client ethClient;
+  bool data = false;
   final testAddress = "0x8717A44ec01bFd229B732EBBD048fAD2ceA67F8b";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      backgroundColor: Vx.gray300,
+      body: ZStack([
+        VxBox()
+            .blue600
+            .size(context.screenWidth, context.safePercentHeight * 30)
+            .make(),
+        VStack([
+          (context.percentHeight * 10).heightBox,
+          "\$BrokeOnCryptoToken".text.xl4.white.bold.makeCentered().py16(),
+          (context.percentHeight * 5).heightBox,
+          VxBox(
+                  child: VStack([
+            "Balance".text.gray700.xl2.semiBold.makeCentered(),
+            10.heightBox,
+            data
+                ? "\$1".text.bold.xl6.makeCentered()
+                : CircularProgressIndicator().centered(),
+          ]))
+              .p16
+              .white
+              .rounded
+              .shadowXl
+              .size(context.screenWidth, context.safePercentHeight * 18)
+              .make()
+              .p16(),
+          30.heightBox,
+          HStack([
+            StyledTextButton(),
+            StyledTextButton(),
+            StyledTextButton(),
+
+          ])
+        ])
+      ]),
     );
+  }
+}
+
+class StyledTextButton extends StatelessWidget {
+  const StyledTextButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.blue,
+        ),
+        icon: Icon(
+          Icons.refresh,
+          color: Colors.white,
+        ),
+        label: "Refresh".text.white.make());
   }
 }
